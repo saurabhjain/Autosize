d181be279b40c13b9e40d0302fdcc6152b194f43
Saurabh Jain <saurabhjain.net@gmail.com>
2011-11-13 18:05:23 -0800

    Last commit was for - Autozise feature, this one - Reszing views through code

--- Autosize/AutosizeViewController.m
+++ Autosize/AutosizeViewController.m
@@ -9,9 +9,43 @@
 #import "AutosizeViewController.h"
 
 @implementation AutosizeViewController
+@synthesize button1;
+@synthesize button2;
+@synthesize button3;
+@synthesize button4;
+@synthesize button5;
+@synthesize button6;
+
+- (void)willAnimateSecondHalfOfRotationFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation duration:(NSTimeInterval)duration {
+    UIInterfaceOrientation toOrientation = self.interfaceOrientation;
+    
+    if(toOrientation == UIInterfaceOrientationPortrait || toOrientation == UIInterfaceOrientationPortraitUpsideDown) {
+        button1.frame = CGRectMake(20, 20, 125, 125);
+        button2.frame = CGRectMake(175, 20, 125, 125);
+        button3.frame = CGRectMake(20, 168, 125, 125);
+        button4.frame = CGRectMake(175, 168, 125, 125);
+        button5.frame = CGRectMake(20, 315, 125, 125);
+        button6.frame = CGRectMake(175, 315, 125, 125);
+    }
+    else
+    {
+        button1.frame = CGRectMake(20, 20, 125, 125);
+        button2.frame = CGRectMake(20, 155, 125, 125);
+        button3.frame = CGRectMake(177, 20, 125, 125);
+        button4.frame = CGRectMake(177, 155, 125, 125);
+        button5.frame = CGRectMake(328, 20, 125, 125);
+        button6.frame = CGRectMake(328, 155, 125, 125);
+    }
+}
 
 - (void)dealloc
 {
+    [button1 release];
+    [button2 release];
+    [button3 release];
+    [button4 release];
+    [button5 release];
+    [button6 release];
     [super dealloc];
 }
 
